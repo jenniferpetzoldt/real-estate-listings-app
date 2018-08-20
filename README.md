9ce98c41a1f7d09360a3c41038000034f2c92789
Jennifer Petzoldt <j.petzoldt@gmail.com>
2018-08-19 21:40:32 -0500

    added drop down lists for type and image

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Real Estate Listing App
 
+##Built With
+* AngularJS
+* Node.js
+* Express
+* HTML
+* CSS
+
+## Getting Started
+These instructions will create a copy of the project for you to utilize on your local machine for development and testing purposes. 
+
+###Installing
+Steps to get the develoment environment running.
+
+Download this project.
+npm install
+npm start
+Open within your code editing tool.
+
+###Completed Features
+- [ ] Add new listings
+- [ ] Listings are sorted by rentals and for sale
+- [ ] Listings are deletable
+- [ ] Dropdown lists within form 
+
+###Next Steps
+Features to be added:
+- [ ] Sorting listings by price
+- [ ] 
+- [ ]
+- [ ]
+
+
+##Authors
+Jennifer Petzoldt
--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,5 +1,7 @@
 myApp.controller('HomeController', function ($http) {
     const vm = this;
+    vm.types = ["rent", "sale"];
+    vm.images = ["classic-flats,jpg", "haunted.png", "older.jpg", "rental.jpg", "rental2.jpg", "shiny.jpg", "stony.jpg"];
     console.log('NG in home');
     vm.addListing = function (listingToAdd) {
         console.log('adding listing to server', listingToAdd);
@@ -9,8 +11,18 @@ myApp.controller('HomeController', function ($http) {
             data: listingToAdd
         }).then(function (response) {
             console.log('response from POST', response);
+            clearInputs();
         }).catch(function (error) {
             console.log('POST error', error);
         });
-    };// end vm.addListing   
+    };// end vm.addListing 
+
+    function clearInputs(){
+        vm.listingToAdd.cost = '';
+        vm.listingToAdd.sqft = '';
+        vm.listingToAdd.type = '';
+        vm.listingToAdd.city = '';
+        vm.listingToAdd.image_path = ''
+    };
+
 });// end HomeController
